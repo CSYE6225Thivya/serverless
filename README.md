d04be13c9f1248e3099bcf63d4064bc5a42a27c8
Thivya Dhanasegaran <dhanasegaran.t@northeastern.edu>
2024-03-27 01:59:01 -0400

    Added Cloud functions

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# serverless
+# Serverless function
+## Assignment - 7
+
+This repository contains code for a serverless infrastructure setup using Google Cloud Functions.
+
+## Overview
+
+The serverless infrastructure utilizes Google Cloud Functions to handle tasks triggered by events. In this setup, a Cloud Function is triggered by a Pub/Sub message whenever a new user account is created. The Cloud Function then performs two main tasks:
+
+1. Email Verification**: It sends an email to the user containing a verification link. This link expires after 2 minutes to ensure security and validity. The email is sent using Mailgun service.
+
+2. Database Update**: It updates a Cloud SQL database to track the emails sent, including details such as the recipient's email, verification token, and the expiration time of the verification link.
+
+
+## Setup
+
+1. Dependencies: Have to ensure the necessary dependencies installed. This includes the Google Cloud SDK ('gcloud') and Node.js.
+
+2. Environment Variables: Set up environment variables for your Google Cloud project, Mailgun API key, and database credentials. 
+
+3. Deployment: Deploying Cloud Function from GCP Bucket
+
+To deploy the Cloud Function from the code stored in a GCP bucket, follow these steps:
+
+1. **Access the Bucket**: 
+   - Navigate to the Google Cloud Storage section of the GCP Console.
+   - Create a new bucket named "my-serverless-function"
+   - Upload the Cloud Function code file as email.zip.
+
+2. **Download Cloud Function Code**: 
+   - Click on the Cloud Function code file stored in the bucket.
+   - Choose the option to download the file to your local machine.
+
+3. **Deploy Cloud Function**:
+   - Navigate to the Google Cloud Functions section of the GCP Console.
+   - Click on the "Create function" button.
+   - Provide a name and description for your Cloud Function.
+   - Choose the option to upload a zip file.
+   - Upload the Cloud Function code file that you downloaded from the bucket.
+   - Configure the trigger for your Cloud Function (e.g., HTTP trigger, Pub/Sub trigger).
+   - Click on the "Create" button to deploy the Cloud Function.
+   - Specify the Cloud Storage bucket and object name in the Terraform configuration for the Cloud Function resource. 
+
+
+## Usage
+
+To use this infrastructure:
+
+1. Create a new user account in your web application.
+2. The Cloud Function will be triggered automatically upon new user creation.
+3. An email containing a verification link will be sent to the user's email address.
+4. The Cloud SQL database will be updated to track the email sent.
+
+
---
+++ email.js
@@ -0,0 +1,149 @@
+const { PubSub } = require('@google-cloud/pubsub');
+const { v4: uuidv4 } = require('uuid');
+const moment = require('moment');
+const axios = require('axios');
+const mailgun = require('mailgun-js');
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.HOST,
+  dialect: 'mysql',
+  timezone: '+00:00',
+});
+// Define User model
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4, 
+      primaryKey: true,
+      allowNull: false,
+    },
+    // Attributes for the User model
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "First name cannot be empty",
+        },
+      },
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Last name cannot be empty",
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Password cannot be empty",
+        },
+      },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: "Invalid email format",
+        },
+      },
+    },
+    account_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    account_updated: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    isVerified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false, 
+    },
+    token: {
+      type: DataTypes.UUID, 
+      defaultValue: null, 
+    },
+    validity_time: {
+      type: DataTypes.DATE, 
+      defaultValue: null, 
+    },
+  },
+  {
+    timestamps: false 
+  })
+
+const mg =  mailgun({
+  apiKey: '71bd69a940f3b74a0b6e17218a5e6bed-309b0ef4-201c27e7',
+  domain: 'mycloudwebapp.me' 
+});
+
+
+// Create a Pub/Sub client
+const pubsub = new PubSub();
+const currentTime = new Date();
+
+exports.handleNewUserAccount = async (event, context) => {
+  const pubsubMessage = event.data ? JSON.parse(Buffer.from(event.data, 'base64').toString()) : {};
+  const { id: userId, username: userEmail } = pubsubMessage;
+  // Generate verification token
+  const verificationToken = uuidv4();
+  // Send email verification link to the user
+  await sendVerificationEmail(userEmail, verificationToken);
+  const validityTime = new Date(Date.now() + 2 * 60 * 1000);
+  await updateDatabase(userEmail, verificationToken, validityTime);
+  console.log(`Verification email sent to ${userEmail}`);
+};
+
+
+async function sendVerificationEmail(userEmail, verificationToken) {
+  try {
+    const apiKey = "71bd69a940f3b74a0b6e17218a5e6bed-309b0ef4-201c27e7";
+    const domain = "mycloudwebapp.me";
+    const sender = "mail@mycloudwebapp.me";
+    const data = {
+      from: sender,
+      to: userEmail,
+      subject: 'Email Verification',
+       text: `Please click the following link to verify your email: ${generateVerificationLink(userEmail, verificationToken)}`,
+    };
+    const body = await mg.messages().send(data);
+    console.log('Mailgun response:', body);
+  } catch (error) {
+    console.error('Error sending verification email:', error.message);
+    throw error; 
+  }
+}
+
+function generateVerificationLink(userEmail,verificationToken) {
+    const baseUrl = 'http://mycloudwebapp.me:8080';
+    const verificationLink = `${baseUrl}/verify-email?username=${userEmail}&token=${verificationToken}`; 
+    return verificationLink;
+  }
+
+  // Function to update db
+async function updateDatabase(userEmail, verificationToken, validityTime) {
+  try {
+    const [rowsAffected] = await User.update(
+      { token: verificationToken, validity_time: validityTime },
+      { where: { username: userEmail } }
+    );
+    console.log(`Updated ${rowsAffected} row(s)`);
+  } catch (error) {
+    console.error('Error updating database:', error);
+    throw error;
+  }
+}
